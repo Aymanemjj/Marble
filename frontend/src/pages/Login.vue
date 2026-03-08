c7f1fc95e8687a9a417eda9cf29f2fa8d056ff4f
Aymane <aymanemj@proton.me>
2026-03-08 16:16:20 +0000

    Login page

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,17 @@
+<script setup>
+import BasicInput from '../components/BasicInput.vue';
+import authBg from '../assets/authenticationBG.jpg'
+import BasicButton from '../components/BasicButton.vue';
+</script>
+
+<template>
+  <main :style="{ backgroundImage: `url(${authBg})` }" class="bg-cover bg-center bg-no-repeat grid grid-cols-5 gap-4 min-h-screen">
+    <div class="col-span-2 bg-bg h-fit p-4">
+      <form class="grid grid-cols-2 gap-4">
+        <BasicInput label="Email" name="email" type="email" class="col-span-2"/>
+        <BasicInput label="Password" name="password" type="password" class="col-span-2" />
+        <BasicButton label="Login" type="submit" class="col-span-2" />
+      </form>
+    </div>
+  </main>
+</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Register from './pages/Register.vue'
+import Login from './pages/Login.vue'
 
 
 const routes = [
   { path: '/register', component: Register },
+  { path: '/login', component: Login },
 ]
 
 export const router = createRouter({
