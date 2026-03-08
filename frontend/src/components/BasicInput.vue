12c27400c93249a08e1b2afdb32f55329133e35b
Aymane <aymanemj@proton.me>
2026-03-08 14:10:18 +0000

    Basic components for a form

---
+++ frontend/src/components/BasicInput.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['label', 'name', 'type'])
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <label :label="name" class="font-bold text-2xl">{{ label }}</label>
+        <input :type="type" :name="name" class="border border-0.5 border-text bg-asscent h-12 text-2xl">
+    </div>
+</template>
