ad71d60286ab97fdc327511db8b6e223f63c27b1
Aymane <aymanemj@proton.me>
2026-04-01 12:42:43 +0100

    Routes and middle whare in api.php for logout and profile

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,15 @@ use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::middleware("auth:sanctum")->group(function () {
+
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logOut'])->name('logout');
+
+
+});
+
+
 
 
 Route::post('/register',[AuthController::class, 'register']);
