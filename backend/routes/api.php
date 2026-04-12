1524c795b8583b504b10922f8f96a54d78e39efb
Aymane <aymanemj@proton.me>
2026-04-12 23:22:41 +0100

    Piece routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,18 +1,27 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\PieceController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware("auth:sanctum")->group(function () {
 
-    Route::get('/profile', [AuthController::class, 'profile']);
+
+    //Authentication
+    Route::get('/profile', [AuthController::class, 'profile'])->name('logout');
     Route::post('/logout', [AuthController::class, 'logOut'])->name('logout');
 
+
+    //Pieces
+    Route::post('/piece/new', [PieceController::class, 'store'])->name('piece-new');
+    Route::put('/piece/{piece}/update', [PieceController::class, 'update'])->name('piece-update');
+    Route::delete('/piece/{piece}/delete', [PieceController::class, 'destroy'])->name('piece-delete');
+    Route::get('/piece/{piece}', [PieceController::class, 'show'])->name('piece-details');
 });
 
 
 
-
-Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login']);
+//Authentication
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
