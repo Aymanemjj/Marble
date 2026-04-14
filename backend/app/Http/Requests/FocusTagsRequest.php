12c62d98d644a401daaa98feb2ecb7e61c75d2aa
Aymane <aymanemj@proton.me>
2026-04-14 13:54:42 +0100

    FocusTagsRequest
    Request that'll come from the focus page
    a list of tags to filter by

---
+++ backend/app/Http/Requests/FocusTagsRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class FocusTagsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tags'   => 'nullable|array',
+            'tags.*' => 'exists:tags,id',
+        ];
+    }
+
+
+    public function messages(): array
+    {
+        return [
+            'tags.*.exists' => 'One or more selected tags do not exist.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+        $response = response()->json([
+            "success" => false,
+            'message' => 'Erreur de validation',
+            'error' => $errors->messages(),
+        ], 422);
+
+        throw new HttpResponseException($response);
+    }
+}
