0afb95c83c3cdb8a736d7525392001de7658ec1e
Aymane <aymanemj@proton.me>
2026-04-14 13:55:59 +0100

    added a max of 10 on the focusTagRequest

--- backend/app/Http/Requests/FocusTagsRequest.php
+++ backend/app/Http/Requests/FocusTagsRequest.php
@@ -24,7 +24,7 @@ class FocusTagsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'tags'   => 'nullable|array',
+            'tags'   => 'nullable|array|max:10',
             'tags.*' => 'exists:tags,id',
         ];
     }
