2adf5e6649fbe855286ce358d0f3d094c27d7641
Aymane <aymanemj@proton.me>
2026-04-14 14:38:55 +0100

    getPieces method in FocusService to get 10 random pieces that include a list of tags

--- backend/app/Services/FocusService.php
+++ backend/app/Services/FocusService.php
@@ -2,6 +2,10 @@
 
 namespace App\Services;
 
+use App\Dtos\PieceDTO;
+use App\Models\Piece;
+use Illuminate\Support\Facades\DB;
+
 class FocusService
 {
     /**
@@ -11,4 +15,31 @@ class FocusService
     {
         //
     }
+
+
+    public function getPieces($request)
+    {
+        $validated = $request->validated();
+        if (is_null($validated['tags'])) {
+            $pieces = Piece::inRandomOrder()->take(10)->get();
+            return response()->json([
+                'success' => true,
+                'message' => '10 random pieces',
+                'data' => PieceDTO::collection($pieces)
+            ], 200);
+        }
+        $Rtags = $validated['tags'];
+
+        $pieces = Piece::whereHas('tags', function ($query) use ($Rtags) {
+            $query->whereIn('tags.id', $Rtags);
+        })->inRandomOrder()->take(10)->get();
+
+        return response()->json([
+            'success' => true,
+            'message' => '10 random pieces',
+            'data' => PieceDTO::collection($pieces)
+        ], 200);
+    }
+
+
 }
