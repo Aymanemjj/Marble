0c274fb5671cb92db7cd1150edb5daaa89cb62ba
Aymane <aymanemj@proton.me>
2026-04-15 22:51:30 +0100

    Made a Piece card component for reusability removed a forgoten console.log in PieceService.js

---
+++ frontend/src/components/PieceCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import router from '../router';
+import { pieceDetails } from '../services/PieceService';
+
+defineProps(['image'])
+</script>
+
+<template>
+    <div class="cursor-pointer">
+        <div @click="router.push(`/piece/${image.id}`)">
+        <img :src="image.path" class="w-full h-auto transition-transform duration-300 group-hover:scale-90" >
+        </div>
+
+        <div
+            class="absolute inset-0 flex  self-end justify-self-end opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-fit w-fit ">
+            <button class="bg-bg hover:bg-text text-text  hover:text-bg px-4 py-2 border border-text hover:border-bg hover:underline cursor-pointer" @click="console.log('save')">
+                Save
+            </button>
+        </div>
+    </div>
+</template>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,13 +2,13 @@
 import axiosClient from '../axios';
 import { onMounted, onUnmounted, ref, watchEffect } from 'vue';
 import { loadPieces } from '../services/PieceService';
+import PieceCard from '../components/PieceCard.vue';
 
 const IMAGES = ref([])
 const scrollComponent = ref(null)
 
-const loadMorePieces =async ()=>{
-    let newPieces = await loadPieces()
-
+const loadMorePieces = async () => {
+    let newPieces = await loadPieces()    
     IMAGES.value.push(...newPieces)
 }
 
@@ -19,20 +19,17 @@ onMounted(async () => {
 
 })
 
-onUnmounted(()=>{
+onUnmounted(() => {
     window.removeEventListener('scroll', handleScroll)
 })
- const handleScroll = (e)=>{
-    
+const handleScroll = (e) => {
+
     let element = scrollComponent.value
-    if( element.getBoundingClientRect().bottom < window.innerHeight - 700){
+    if (element.getBoundingClientRect().bottom < window.innerHeight - 700) {
 
         loadMorePieces()
     }
- }
-
-
-
+}
 
 
 </script>
@@ -41,16 +38,8 @@ onUnmounted(()=>{
     <main class="columns-5 gap-4 " ref="scrollComponent">
 
         <div v-for="image in IMAGES" class="break-inside-avoid mb-4 relative group overflow-hidden bg-text">
-
-            <img :src="image.path" class="w-full h-auto transition-transform duration-300 group-hover:scale-90">
-
-            <div
-                class="absolute inset-0 flex items-end justify-end opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <button class="bg-bg text-text px-4 py-2 border border-text cursor-pointer" @click="">
-                    Save
-                </button>
-            </div>
+            <PieceCard :image="image"/>
         </div>
-        
+
     </main>
 </template>
