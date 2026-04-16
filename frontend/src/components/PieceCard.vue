d536be016b10726006d9d8e74afe1b5961fcfa67
Aymane <aymanemj@proton.me>
2026-04-16 17:26:03 +0100

    Tried to fix the Piece creator info glitch, didn't work
    THe creator info of a piece if no space left wraps to the next column
    resulting on a wiered and harmless glitch

--- frontend/src/components/PieceCard.vue
+++ frontend/src/components/PieceCard.vue
@@ -1,33 +1,43 @@
 <script setup>
-import router from '../router';
+import router from "../router";
 
-defineProps(['image'])
+defineProps(["image"]);
 </script>
 
 <template>
-    <div class="flex flex-col flex-nowrap  mb-4 gap-2">
-        <div class="cursor-pointer break-inside-avoid  relative group overflow-hidden bg-text">
-            <div @click="router.push(`/piece/${image.id}`)">
-                <img :src="image.path" class="w-full h-auto transition-transform duration-300 group-hover:scale-90 group-active:scale-80">
-            </div>
+  <div class="flex flex-col flex-nowrap mb-4 gap-2">
+    <div class="cursor-pointer break-inside-avoid relative group overflow-hidden bg-text">
+      <div @click="router.push(`/piece/${image.id}`)">
+        <img
+          :src="image.path"
+          class="w-full h-auto transition-transform duration-300 group-hover:scale-90 group-active:scale-80"
+        />
+      </div>
 
-            <div
-                class="absolute inset-0 flex  self-end justify-self-end opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-fit w-fit ">
-                <button
-                    class="bg-bg hover:bg-text text-text  hover:text-bg px-4 py-2 border border-text hover:border-bg hover:underline cursor-pointer"
-                    @click="console.log('save')">
-                    Save
-                </button>
-            </div>
-        </div>
-        <div class="flex gap-4 ">
-            <div class=" aspect-square w-8">
-            <img :src="image.path" alt="" class="bg-contain">
-            </div>
-            <div class="flex flex-col">
-                <h2>{{ image.title }}</h2>
-                <h3 @click="router.push(`/creator/${image.creator.id}`)" class="cursor-pointer hover:underline"> U/{{ image.creator.firstname }} {{ image.creator.firstname }}</h3>
-            </div>
-        </div>
+      <div
+        class="absolute inset-0 flex self-end justify-self-end opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-fit w-fit"
+      >
+        <button
+          class="bg-bg hover:bg-text text-text hover:text-bg px-4 py-2 border border-text hover:border-bg hover:underline cursor-pointer"
+          @click="console.log('save')"
+        >
+          Save
+        </button>
+      </div>
     </div>
+    <div class="flex gap-4">
+      <div class="aspect-square w-8">
+        <img :src="image.path" alt="" class="bg-contain" />
+      </div>
+      <div class="flex flex-col">
+        <h2>{{ image.title }}</h2>
+        <h3
+          @click="router.push(`/creator/${image.creator.id}`)"
+          class="cursor-pointer hover:underline"
+        >
+          U/{{ image.creator.firstname }} {{ image.creator.firstname }}
+        </h3>
+      </div>
+    </div>
+  </div>
 </template>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,45 +1,37 @@
 <script setup>
-import axiosClient from '../axios';
-import { onMounted, onUnmounted, ref, watchEffect } from 'vue';
-import { loadPieces } from '../services/PieceService';
-import PieceCard from '../components/PieceCard.vue';
+import axiosClient from "../axios";
+import { onMounted, onUnmounted, ref, watchEffect } from "vue";
+import { loadPieces } from "../services/PieceService";
+import PieceCard from "../components/PieceCard.vue";
 
-const IMAGES = ref([])
-const scrollComponent = ref(null)
+const IMAGES = ref([]);
+const scrollComponent = ref(null);
 
 const loadMorePieces = async () => {
-    let newPieces = await loadPieces()    
-    IMAGES.value.push(...newPieces)
-}
+  let newPieces = await loadPieces();
+  IMAGES.value.push(...newPieces);
+};
 
 onMounted(async () => {
-    IMAGES.value = (await axiosClient.get('/index')).data.data.pieces;
-
-    window.addEventListener('scroll', handleScroll)
-
-})
+  IMAGES.value = (await axiosClient.get("/index")).data.data.pieces;
+  window.addEventListener("scroll", handleScroll);
+});
 
 onUnmounted(() => {
-    window.removeEventListener('scroll', handleScroll)
-})
+  window.removeEventListener("scroll", handleScroll);
+});
 const handleScroll = (e) => {
-
-    let element = scrollComponent.value
-    if (element.getBoundingClientRect().bottom < window.innerHeight - 700) {
-
-        loadMorePieces()
-    }
-}
-
-
+  let element = scrollComponent.value;
+  if (element.getBoundingClientRect().bottom < window.innerHeight - 700) {
+    loadMorePieces();
+  }
+};
 </script>
 
 <template>
-    <main class="columns-5 gap-4 " ref="scrollComponent">
-
-        <div v-for="image in IMAGES" class="">
-            <PieceCard :image="image"/>
-        </div>
-
-    </main>
+  <main class="columns-5 gap-4" ref="scrollComponent">
+    <div v-for="image in IMAGES" class="">
+      <PieceCard :image="image" />
+    </div>
+  </main>
 </template>
