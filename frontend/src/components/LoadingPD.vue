a26b9d0722fcc1de58086ba8f2bcb0a997d847a1
Aymane <aymanemj@proton.me>
2026-04-16 14:45:05 +0100

    Loading sckelaton layout component | ContactLinks | PieceCard

---
+++ frontend/src/components/LoadingPD.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <main class="flex flex-col gap-16 ">
+        <div class="flex flex-col gap-4">
+            <!-- image -->
+            <div class="w-full size-96 bg-text animate-pulse"></div>
+
+            <div class="font-extrabold flex justify-between">
+                <!-- title and artist -->
+                <div class="text-2xl w-24 h-4 bg-text animate-pulse">
+                </div>
+                <div class="text-2xl w-20 h-4 bg-text animate-pulse">
+                </div>
+            </div>
+
+            <div class="flex justify-between">
+                <div class="text-2xl w-12 h-4 bg-text animate-pulse">
+                </div>
+                <div class="flex gap-2">
+                    <div class="text-2xl w-12 h-4 bg-text animate-pulse">
+                    </div>
+                    <div class="text-2xl w-12 h-4 bg-text animate-pulse">
+                    </div>
+                    <div class="text-2xl w-12 h-4 bg-text animate-pulse">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-5 gap-4">
+            <div class="flex flex-col justify-between gap-12">
+                <div class="flex flex-col gap-4">
+                    <div class="text-2xl  aspect-square bg-text animate-pulse">
+                    </div>
+                    <div class="text-2xl w-16 h-4 bg-text animate-pulse">
+                    </div>
+                </div>
+                <div class="flex flex-col text-2xl font-bold gap-4">
+                    <div class="text-2xl w-32 h-4 bg-text animate-pulse">
+                    </div>
+                    <div class="text-2xl w-32 h-4 bg-text animate-pulse">
+                    </div>
+                    <div class="text-2xl w-32 h-4 bg-text animate-pulse">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-start-3 col-span-3 font-bold text-3xl text-justify w-full flex flex-col gap-8">
+                <div class="text-2xl w-full h-4 bg-text animate-pulse">
+                </div>
+                <div class="text-2xl w-full h-4 bg-text animate-pulse">
+                </div>
+                <div class="text-2xl w-full h-4 bg-text animate-pulse">
+                </div>
+                <div class="text-2xl w-full h-4 bg-text animate-pulse">
+                </div>
+                <div class="text-2xl w-full h-4 bg-text animate-pulse">
+                </div>
+            </div>
+        </div>
+    </main>
+
+</template>
--- frontend/src/components/PieceCard.vue
+++ frontend/src/components/PieceCard.vue
@@ -1,21 +1,33 @@
 <script setup>
 import router from '../router';
-import { pieceDetails } from '../services/PieceService';
 
 defineProps(['image'])
 </script>
 
 <template>
-    <div class="cursor-pointer">
-        <div @click="router.push(`/piece/${image.id}`)">
-        <img :src="image.path" class="w-full h-auto transition-transform duration-300 group-hover:scale-90" >
-        </div>
+    <div class="flex flex-col mb-4 gap-2">
+        <div class="cursor-pointer break-inside-avoid  relative group overflow-hidden bg-text">
+            <div @click="router.push(`/piece/${image.id}`)">
+                <img :src="image.path" class="w-full h-auto transition-transform duration-300 group-hover:scale-90">
+            </div>
 
-        <div
-            class="absolute inset-0 flex  self-end justify-self-end opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-fit w-fit ">
-            <button class="bg-bg hover:bg-text text-text  hover:text-bg px-4 py-2 border border-text hover:border-bg hover:underline cursor-pointer" @click="console.log('save')">
-                Save
-            </button>
+            <div
+                class="absolute inset-0 flex  self-end justify-self-end opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-fit w-fit ">
+                <button
+                    class="bg-bg hover:bg-text text-text  hover:text-bg px-4 py-2 border border-text hover:border-bg hover:underline cursor-pointer"
+                    @click="console.log('save')">
+                    Save
+                </button>
+            </div>
+        </div>
+        <div class="flex gap-4">
+            <div class=" aspect-square w-8">
+            <img :src="image.path" alt="" class="bg-contain">
+            </div>
+            <div class="flex flex-col">
+                <h2>{{ image.title }}</h2>
+                <h3> U/{{ image.creator.firstname }} {{ image.creator.firstname }}</h3>
+            </div>
         </div>
     </div>
 </template>
