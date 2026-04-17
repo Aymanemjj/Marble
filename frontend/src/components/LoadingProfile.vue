604545409741cfaac8f835ad6a4db1926bd105fa
Aymane <aymanemj@proton.me>
2026-04-17 15:16:56 +0100

    Profile page and it's loading skeleton

---
+++ frontend/src/components/LoadingProfile.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <div class="grid grid-cols-5 gap-4">
+            <div class="flex flex-col gap-8">
+                <div class="flex flex-col gap-2">
+                    <div class="bg-text animate-pulse aspect-square"></div>
+                    <div class="bg-text animate-pulse w-24 h-4"></div>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <div class="bg-text animate-pulse w-20 h-4"></div>
+                    <div class="bg-text animate-pulse w-20 h-4"></div>
+                    <div class="bg-text animate-pulse w-20 h-4"></div>
+
+                </div>
+            </div>
+
+            <div class="col-start-3 col-span-3 flex flex-col gap-4 w-full">
+                <div class="bg-text animate-pulse w-full h-4"></div>
+                <div class="bg-text animate-pulse w-full h-4"></div>
+                <div class="bg-text animate-pulse w-full h-4"></div>
+                <div class="bg-text animate-pulse w-full h-4"></div>
+                <div class="bg-text animate-pulse w-full h-4"></div>
+                <div class="bg-text animate-pulse w-full h-4"></div>
+                <div class="bg-text animate-pulse w-full h-4"></div>
+
+            </div>
+        </div>
+    </main>
+</template>
--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -1,41 +1,74 @@
 <script setup>
-import { useRoute } from 'vue-router';
-import router from '../router';
-import { onMounted, ref } from 'vue';
-import axiosClient from '../axios';
+import { useRoute } from "vue-router";
+import router from "../router";
+import { computed, onMounted, ref } from "vue";
+import axiosClient from "../axios";
+import LoadingProfile from "../components/LoadingProfile.vue";
 
-const id = useRoute().params.id
+const id = useRoute().params.id;
+const type = useRoute().params.creatorType;
 
-const USER = ref(null)
-onMounted(async()=>{
-    USER.value = (await axiosClient.get(`/creator/${id}`))
-})
+const CREATOR = ref(null);
+const isLoading = computed(() => CREATOR.value == null);
+
+onMounted(async () => {
+  CREATOR.value = (await axiosClient.get(`/creator/${type}/${id}`)).data.data;
+
+  console.log(CREATOR.value);
+
+});
+
 
 </script>
 
 <template>
-    <main>
-        <div class="grid grid-cols-5 gap-4">
-            <div class="flex flex-col justify-between">
-                <div>
-                    <img class="w-fit aspect-square"
-                        src="http://localhost:8000/storage/pieces/n00J88tZSeN1LRILtG8EEODRcDLNe3a4VH2ImWgH.jpg" alt="">
-                    <h3 class="font-extrabold text-2xl cursor-pointer hover:underline" @click="router.push(`/creator/2`)">Artist</h3>
-                </div>
-                <div class="flex flex-col text-2xl font-bold">
-                    <h4 class="hover:underline cursor-pointer">CONTACT</h4>
-                    <h4 class="hover:underline">CONTACT</h4>
-                    <h4 class="hover:underline">CONTACT</h4>
-                </div>
-            </div>
+  <LoadingProfile v-if="isLoading" />
+  <main v-else class="grid grid-cols-5 auto-rows-[25vh] gap-4">
+    <div class="h-full aspect-square">
+      <img :src="CREATOR.profile.picture" alt="" class="">
+    </div>
+    <div class="flex gap-2 items-end">
+      <h3 class="text-2xl font-bold">{{ CREATOR.firstname.toUpperCase() }}</h3>
+      <h3 v-if="CREATOR.middlename != 'None'" class="text-2xl font-bold">{{ CREATOR.middlename.toUpperCase() }}</h3>
+      <h3 class="text-2xl font-bold">{{ CREATOR.lastname.toUpperCase() }}</h3>
+    </div>
+    <div class="row-start-2 col-span-2">
+      <p class="text-3xl font-bold w-full text-justify">{{ CREATOR.profile.biography.toUpperCase() }}</p>
+    </div>
 
-            <div class="col-start-3 col-span-3 font-bold text-3xl text-justify w-full">
-                <p>There is no strife, no prejudice, no national conflict in outer space as yet. Its hazards are hostile
-                    to us all. Its conquest deserves the best of all mankind, and its opportunity for peaceful
-                    cooperation many never come again. But why, some say, the moon? Why choose this as our goal? And
-                    they may well ask why climb the highest mountain? Why, 35 years ago, fly the Atlantic? Why does Rice
-                    play Texas?</p>
-            </div>
-        </div>
-    </main>
+    <div class="col-span-3 row-span-3 w-full h-full">
+      <img :src="CREATOR.profile.banner" alt="" class="w-full object-contain max-h-full ">
+    </div>
+
+    <div v-if="CREATOR.administered">
+      <a :href="contact.path" v-for="contact in CREATOR.contacts">{{ contact.name }}</a>
+    </div>
+    <div class="row-start-4 row-span-2 col-span-2 w-full h-full">
+      <img :src="CREATOR.profile.banner" alt="" class="w-full object-contain max-h-full ">
+    </div>
+    <div class="row-start-4 col-start-3 items-end flex">
+      <h2 class="text-2xl font-bold">{{ CREATOR.firstname.toUpperCase() }}</h2>
+    </div>
+    <div class="row-start-5 col-start-3 col-span-3 flex items-end">
+      <p class="text-2xl font-bold text-justify">{{ CREATOR.profile.biography.toUpperCase() }}</p>
+    </div>
+
+    <div class="row-start-6 col-start-1 items-end flex">
+      <h2 class="text-2xl font-bold">{{ CREATOR.firstname.toUpperCase() }}</h2>
+    </div>
+    <div class="row-start-7 col-start-1 col-span-3 flex items-end">
+      <p class="text-2xl font-bold text-justify">{{ CREATOR.profile.biography.toUpperCase() }}</p>
+    </div>
+
+    <div class="row-start-6 row-span-2 col-span-2 col-start-4 w-full h-full">
+      <img :src="CREATOR.profile.banner" alt="" class="w-full object-contain max-h-full ">
+    </div>
+
+    <div class="row-start-8 col-start-4 col-span-2">
+      <button @click="router.push(`/creator/${type}/${id}/gallery`)" class="border-2 border-text text-2xl font-bold h-1/4 text-left w-full cursor-pointer hover:bg-text hover:text-bg hover:border-bg pl-2">
+        View full gallery
+      </button>
+
+    </div>
+  </main>
 </template>
