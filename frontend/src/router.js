1df48bc7a10d6b5496c4ae6ff0820adb7a22b4c3
Aymane <aymanemj@proton.me>
2026-04-20 09:58:03 +0100

    changes to the route for the focus pages
    from /focus to /focus/settings for the seeting up
    and /focus/piece/:id for the actual piece

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,11 +3,12 @@ import Register from './pages/Register.vue'
 import Login from './pages/Login.vue'
 import Profile from './pages/Profile.vue'
 import Artists from './pages/Artists.vue'
-import Focus from './pages/Focus.vue'
+import FocusSettings from './pages/FocusSettings.vue'
 import About from './pages/About.vue'
 import Home from './pages/Home.vue'
 import PieceDetails from './pages/PieceDetails.vue'
 import Gallery from './pages/Gallery.vue'
+import FocusPiece from './pages/FocusPiece.vue'
 
 
 const routes = [
@@ -17,7 +18,8 @@ const routes = [
   { path: '/creator/:creatorType/:id', component: Profile },
   { path: '/creator/:creatorType/:id/gallery', component: Gallery },
   { path: '/artists', component: Artists },
-  { path: '/focus', component: Focus },
+  { path: '/focus/off', component: FocusSettings },
+  { path: '/focus/settings', component: FocusPiece },
   { path: '/about', component: About },
   { path: '/piece/:id', component: PieceDetails, props: true },
 
