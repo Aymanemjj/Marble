443b1ab778c601fac005bab8062afff7a47cc1ac
Aymane <aymanemj@proton.me>
2026-04-21 09:33:14 +0100

    AdminController & AdmionService
    Methods:
    	-BanUser
    	-UnbanUser
    	-Statistics

--- backend/app/Http/Controllers/Api/AdminController.php
+++ backend/app/Http/Controllers/Api/AdminController.php
@@ -3,12 +3,50 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Services\AdminService;
+use Exception;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function __construct(private AdminService $service) {}
 
-    
+    public function banUser(User $user)
+    {
+        try {
+            return $this->service->banUser($user);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+
+    public function unbanUser(User $user)
+    {
+        try {
+            return $this->service->unbanUser($user);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+
+    public function statistics()
+    {
+        try {
+            return $this->service->getStatistics();
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
