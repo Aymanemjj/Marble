4fe20849458a4309c0b22d480c97244d64f57649
Aymane <aymanemj@proton.me>
2026-04-29 12:01:54 +0100

    Before each navigation checks for meta

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -9,19 +9,22 @@ import Home from './pages/Home.vue'
 import PieceDetails from './pages/PieceDetails.vue'
 import Gallery from './pages/Gallery.vue'
 import FocusPiece from './pages/FocusPiece.vue'
-
+import UnAuthenticated from './pages/UnAuthenticated.vue'
+import { useAuthStore } from './stores/useAuthStore'
 
 const routes = [
-  { path: '/', component: Home, name:'Home' , props: true},
-  { path: '/register', component: Register },
-  { path: '/login', component: Login },
+  { path: '/', component: Home, name: 'Home', props: true },
+  { path: '/register', component: Register, meta: { guestOnly: true } },
+  { path: '/login', component: Login, meta: { guestOnly: true } },
   { path: '/creator/:creatorType/:id', component: Profile },
   { path: '/creator/:creatorType/:id/gallery', component: Gallery },
   { path: '/artists', component: Artists },
-  { path: '/focus/off', component: FocusSettings },
-  { path: '/focus/settings', component: FocusPiece },
+  { path: '/focus/settings', component: FocusSettings, meta: { requiresAuth: true } },
+  { path: '/focus/piece', component: FocusPiece, meta: { requiresAuth: true } },
   { path: '/about', component: About },
   { path: '/piece/:id', component: PieceDetails, props: true },
+
+  { path: '/unauth', component: UnAuthenticated, meta: { guestOnly: true } }
 
 ]
 
@@ -29,5 +32,18 @@ export const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore();
+
+  if (to.meta.requiresAuth && !auth.isAuthenticated) {
+    next('/unauth');
+  } else if (to.meta.guestOnly && auth.isAuthenticated) {
+    next('/'); 
+  } else {
+    next();
+  }
+});
 
 export default router
