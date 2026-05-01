c7f023de3b86761434cc0841cfa53f93607f7879
Aymane <aymanemj@proton.me>
2026-05-01 20:55:33 +0100

    Modified the creator gallery and piece details gallery now shows both pieces and collages
    piece details now shows edit and delete button if your the creator of the piece

--- frontend/src/pages/Gallery.vue
+++ frontend/src/pages/Gallery.vue
@@ -1,10 +1,12 @@
 <script setup>
+import CollageCover from "../components/CollageCover.vue";
+import DisplayException from "../components/DisplayException.vue";
 import Loading from "../components/Loading.vue";
 import PieceCover from "../components/PieceCover.vue";
 import { useGallery } from "../composables/useGallery";
 
 
-const {GALLERY, isLoading} = useGallery();
+const { GALLERY, isLoading, selectedTab, hasNoPieces, hasNoCollages } = useGallery();
 
 
 </script>
@@ -13,9 +15,33 @@ const {GALLERY, isLoading} = useGallery();
 
     <Loading v-if="isLoading" />
 
-    <main class="columns-5 gap-4">
-        <div v-for="piece in GALLERY" class="">
-            <PieceCover :piece="piece" />
+    <main v-else class="flex flex-col gap-4">
+
+        <div class="flex divide-x divide-solid divide-text">
+            <button class="w-full font-bold text-2xl hover:underline cursor-pointer"
+                :class="selectedTab == 'Pieces' ? 'border-b' : ''" @click="selectedTab = 'Pieces'">
+                Pieces
+            </button>
+            <button class="w-full font-bold text-2xl hover:underline cursor-pointer"
+                :class="selectedTab == 'Collages' ? 'border-b' : ''" @click="selectedTab = 'Collages'">
+                Collages
+            </button>
+        </div>
+
+        <div v-if="selectedTab == 'Pieces'">
+            <DisplayException v-if="hasNoPieces" :message="'Creator has no Pieces'" :img="'NoPieces'" />
+            <div v-else class="columns-5 gap-4">
+                <div v-for="piece in GALLERY.pieces" class="">
+                    <PieceCover :piece="piece" />
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            <DisplayException v-if="hasNoCollages" :message="'Creator has no public Collages'" :img="'NoCollages'" />
+            <div class="grid grid-cols-5 gap-4 auto-rows-[25vh]">
+                <CollageCover v-for="collage in GALLERY.collages" :key="collage.id" :collage="collage" />
+            </div>
         </div>
     </main>
+
 </template>
--- frontend/src/pages/PieceDetails.vue
+++ frontend/src/pages/PieceDetails.vue
@@ -4,16 +4,18 @@ import router from "../router";
 import ContactLink from "../components/ContactLink.vue";
 import Loading from "../components/Loading.vue";
 import { usePieceDetails } from "../composables/usePieceDetails";
+import { useAuthStore } from "../stores/useAuthStore";
+import { deletePiece, editPiece } from "../services/PieceService";
 
-const {PIECE, isLoading} = usePieceDetails();
-
+const { PIECE, isLoading } = usePieceDetails();
+const auth = useAuthStore()
 </script>
 
 <template>
   <Loading v-if="isLoading" />
   <main v-else class="flex flex-col gap-16">
     <div>
-      <div class="bg-text w-full h-screen flex items-center align-middle">
+      <div class="bg-text w-full  h-220 flex items-center">
         <img :src="PIECE.path" class="w-full object-contain max-h-full" />
       </div>
       <div class="font-extrabold flex justify-between">
@@ -25,40 +27,49 @@ const {PIECE, isLoading} = usePieceDetails();
         </div>
       </div>
       <div class="flex justify-between">
-        <div>
+        <div class="flex gap-4">
           <button
             class="bg-text text-bg hover:underline hover:bg-bg hover:text-text border border-bg hover:border-text px-2 cursor-pointer">
             Save
           </button>
+          <div v-if="PIECE.creator.id === auth.auth.id" class="flex gap-2">
+            <button
+              class="bg-text text-bg hover:underline hover:bg-bg hover:text-text border border-bg hover:border-text px-2 cursor-pointer"
+              @click="editPiece(PIECE.id)">
+              Edit
+            </button>
+            <button
+              class="bg-text text-bg hover:underline hover:bg-red-800 hover:text-bg border border-bg hover:border-text px-2 cursor-pointer"
+              @click="deletePiece(PIECE.id)">
+              Delete
+            </button>
+          </div>
         </div>
         <div class="flex gap-2">
-          <Tag v-for="tag in PIECE.tags" :label="tag" />
+          <Tag v-for="tag in PIECE.tags" :label="tag.name" />
         </div>
       </div>
     </div>
 
-    <div class="grid grid-cols-5 gap-4">
+    <div class="grid grid-cols-5 gap-4 ">
       <div class="flex flex-col justify-between">
         <div>
-          <img class="w-fit aspect-square"
-            src="http://localhost:8000/storage/pieces/n00J88tZSeN1LRILtG8EEODRcDLNe3a4VH2ImWgH.jpg" alt="" />
-          <h3 v-if="PIECE.administered" @click="router.push(`/creator/artist/${PIECE.creator.id}`)"
-            class="cursor-pointer hover:underline font-bold text-2xl">
-            A/{{ PIECE.creator.firstname }} {{ PIECE.creator.lastname }}
+          <img class="w-fit aspect-square" :src="PIECE.creator.profile.picture" alt="" />
+          <h3 class="cursor-pointer hover:underline font-bold text-2xl"
+            @click="router.push(`/creator/${PIECE.administered ? 'artist' : 'user'}/${PIECE.creator.id}`)">
+            {{ PIECE.administered ? 'A' : 'U' }}/{{ PIECE.creator.firstname }} {{ PIECE.creator.lastname }}
           </h3>
-          <h3 v-else @click="router.push(`/creator/user/${PIECE.creator.id}`)" class="cursor-pointer hover:underline font-bold text-2xl">
-            U/{{ PIECE.creator.firstname }} {{ PIECE.creator.lastname }}
-          </h3>
-
         </div>
         <div class="flex flex-col text-2xl font-bold">
-          <ContactLink v-for="contact in PIECE.creator.contacts" :href="contact.link" :name="contact.name" />
+          <ContactLink v-for="contact in PIECE.creator.contacts" :key="contact.name" :href="contact.link"
+            :name="contact.name" />
         </div>
       </div>
 
-      <div class="col-start-3 col-span-3 font-bold text-3xl text-justify w-full">
-        <p>{{ PIECE.story }}</p>
+      <div class="col-start-3 col-span-3 font-bold text-3xl text-justify">
+        <p class="wrap-break-word">{{ PIECE.story }}</p>
       </div>
     </div>
+
   </main>
 </template>
