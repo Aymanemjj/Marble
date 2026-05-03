cb554ba4ba939bb9e4a7c631a876460c45ee0e5b
Aymane <aymanemj@proton.me>
2026-05-04 00:55:56 +0100

    UI change bg color

--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -24,7 +24,7 @@ const submit = async () => {
 <template>
     <main :style="{ backgroundImage: `url(${authBg})` }"
         class="bg-cover bg-center bg-no-repeat grid grid-cols-5 gap-4 min-h-screen">
-        <div class="col-span-2 bg-bg h-fit p-4 self-center mx-4">
+        <div class="col-span-2 bg-asscent h-fit p-4 self-center mx-4">
             <form class="grid grid-cols-2 gap-4" @submit.prevent="submit">
                 <BasicInput :error="errors.firstname" label="Firstname" v-model="data.firstname" name="firstname"
                     type="text" />
